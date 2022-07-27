a365f6635c426e7d3709da9267981557927a3146
EyalSinay <eyalsinay@gmail.com>
2022-07-27 13:12:39 +0300

    fix front url

--- src/utils/global-vars.js
+++ src/utils/global-vars.js
@@ -1,4 +1,4 @@
-const BASE_URL_FRONT = process.env.RUN_STATUS === "development" ? "https://collaclip.herokuapp.com/" : "http://localhost:3000/";
+const BASE_URL_FRONT = process.env.RUN_STATUS === "development" ? "http://localhost:3000/" : "https://collaclip.herokuapp.com/";
 
 const path = __dirname;
 const newPath = path.split("\\");
